5dce6788e7fc050a4ed1048c6549c4716d299d1b
high5technologies <high5technologies@gmail.com>
2022-08-12 21:16:51 -0500

    changes to trigger

--- streamlit-test2/app.py
+++ streamlit-test2/app.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 
 if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
+   chart_data = pd.DataFrame(
+      np.random.randn(20, 3),
+      columns=['a', 'b', 'c'])
 
-    chart_data
+   chart_data
